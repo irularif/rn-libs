b239db4f386f89659ad3f90f5e64984babe04eea
Khoirul <masterirul@gmail.com>
2021-03-14 02:35:00 +0700

    fixing

--- ui/View/index.tsx
+++ ui/View/index.tsx
@@ -1,39 +1,29 @@
 import { useTheme } from "@react-navigation/native";
-import { ITheme } from "../../config/theme";
+import { LinearGradient, LinearGradientProps } from "expo-linear-gradient";
 import { observer } from "mobx-react";
 import React, { ReactNode } from "react";
-import {
-  ScrollView,
-  ScrollViewProps,
-  StyleSheet,
-  View,
-  ViewProps,
-} from "react-native";
+import { StyleSheet, View, ViewProps } from "react-native";
+import { ITheme } from "../../config/theme";
 
-export interface IViewProps extends ViewProps, ScrollViewProps {
-  type?: "View" | "ScrollView";
+export interface IViewProps extends ViewProps, Partial<LinearGradientProps> {
+  type?: "View" | "LinearGradient";
   shadow?: boolean;
-  childRef?: any;
+  viewRef?: any;
   children?: ReactNode;
 }
 
 export default observer((props: IViewProps) => {
-  const { type, shadow, style, childRef } = props;
+  const { type, shadow, style, viewRef } = props;
   const Theme: ITheme = useTheme() as any;
   const shadowStyle = !!shadow ? Theme.shadow : {};
   let cstyle = StyleSheet.flatten([shadowStyle, style]);
 
   switch (type) {
-    case "ScrollView":
+    case "LinearGradient":
       return (
-        <ScrollView
-          {...props}
-          ref={childRef}
-          keyboardShouldPersistTaps={"handled"}
-          style={cstyle}
-        />
+        <LinearGradient {...(props as any)} ref={viewRef} style={cstyle} />
       );
     default:
-      return <View {...props} style={cstyle} ref={childRef} />;
+      return <View {...props} style={cstyle} ref={viewRef} />;
   }
 });
