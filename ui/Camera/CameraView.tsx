7efc8eabe7666a9afd0f65ed2c08209a27e61aa5
IrulArif <masterirul@gmail.com>
2021-05-28 10:02:31 +0700

    fix

--- ui/Camera/CameraView.tsx
+++ ui/Camera/CameraView.tsx
@@ -1,22 +1,18 @@
-import { useTheme } from "@react-navigation/native";
-import { Camera, CameraPictureOptions, CameraProps } from "expo-camera";
-import { observer, useLocalObservable } from "mobx-react";
-import React, { MutableRefObject, useRef } from "react";
-import { Dimensions } from "react-native";
-import { ITheme } from "../../config/theme";
-import Button from "../Button";
-import Icon from "../Icon";
-import Image from "../Image";
-import libsStorage from "../libsStorage";
-import Modal from "../Modal";
-import Spinner from "../Spinner";
-import Text from "../Text";
-import View from "../View";
-import {
-  generateActions,
-  generateCameraView,
-  generateTools,
-} from "./generator";
+import {useTheme} from '@react-navigation/native';
+import {Camera, CameraPictureOptions, CameraProps} from 'expo-camera';
+import {observer, useLocalObservable} from 'mobx-react';
+import React, {MutableRefObject, useEffect, useRef} from 'react';
+import {Dimensions, useWindowDimensions} from 'react-native';
+import {ITheme} from '../../config/theme';
+import Button from '../Button';
+import Icon from '../Icon';
+import Image from '../Image';
+import libsStorage from '../libsStorage';
+import Modal from '../Modal';
+import Spinner from '../Spinner';
+import Text from '../Text';
+import View from '../View';
+import {generateActions, generateCameraView, generateTools} from './generator';
 
 export interface ICameraView extends CameraProps {
   cameraRef?: MutableRefObject<any>;
@@ -44,11 +40,11 @@ export interface ICameraViewProps {
 }
 
 export default observer((props: ICameraView) => {
-  const { visible } = props;
+  const {visible} = props;
   const camViewRef: any = useRef(null);
-  const dim = Dimensions.get("window");
+  const dim = Dimensions.get('window');
   const meta = useLocalObservable(() => ({
-    tempURI: "",
+    tempURI: '',
     loading: false,
   }));
   const cprops = generateCameraView(props, meta, camViewRef);
@@ -66,17 +62,15 @@ export default observer((props: ICameraView) => {
     <Modal
       visible={visible}
       onDismiss={cprops.onDismiss}
-      onRequestClose={cprops.onDismiss}
-    >
+      onRequestClose={cprops.onDismiss}>
       <View
         style={{
-          backgroundColor: "#000000",
+          backgroundColor: '#000000',
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
+          justifyContent: 'center',
+          alignItems: 'center',
           width: dim.width,
-        }}
-      >
+        }}>
         <CameraView {...cprops} meta={meta} />
         <ActionGroupTop {...cprops} meta={meta} />
         <ActionGroupBottom {...cprops} meta={meta} />
@@ -86,13 +80,14 @@ export default observer((props: ICameraView) => {
 });
 
 const CameraView = observer((props: ICameraViewProps) => {
-  const { width, height, cameraProps, meta, camera } = props;
+  const {width, height, cameraProps, meta, camera} = props;
   const Theme: ITheme = useTheme() as any;
+  const dim = useWindowDimensions();
 
   if (!!meta.tempURI) {
     return (
       <Image
-        source={{ uri: meta.tempURI }}
+        source={{uri: meta.tempURI}}
         style={{
           width,
           height,
@@ -103,61 +98,72 @@ const CameraView = observer((props: ICameraViewProps) => {
   }
 
   return (
-    <Camera
-      {...cameraProps}
-      ref={camera}
-      style={{
-        backgroundColor: "red",
-        width,
-        height,
-      }}
-    >
-      {!!meta.loading && (
-        <View
-          style={{
-            backgroundColor: "transparent",
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Spinner
+    <>
+      <Camera
+        {...cameraProps}
+        ref={camera}
+        style={{
+          backgroundColor: 'red',
+          width,
+          height,
+        }}>
+        {!!meta.loading && (
+          <View
             style={{
-              backgroundColor: Theme.colors.card,
-              borderRadius: 8,
-              padding: 10,
-              alignSelf: "center",
-            }}
-          />
-        </View>
-      )}
-    </Camera>
+              backgroundColor: 'transparent',
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Spinner
+              style={{
+                backgroundColor: Theme.colors.card,
+                borderRadius: 8,
+                padding: 10,
+                alignSelf: 'center',
+              }}
+            />
+          </View>
+        )}
+      </Camera>
+      <View
+        style={{
+          height: dim.height,
+          width: dim.width,
+          backgroundColor: '#ffffff00',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      />
+    </>
   );
 });
 
 const ActionGroupTop = observer((props: ICameraViewProps) => {
-  const { meta } = props;
+  const {meta} = props;
   const Theme: ITheme = useTheme() as any;
-  const { ratio, flashMode, switchRatio, switchFlashMode } = generateTools();
+  const {ratio, flashMode, switchRatio, switchFlashMode} = generateTools();
 
   if (!!meta.tempURI) return null;
 
   return (
     <View
       style={{
-        position: "absolute",
+        position: 'absolute',
         left: 0,
         right: 0,
         top: 10,
-        backgroundColor: "transparent",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <View
         shadow
         style={{
@@ -166,12 +172,11 @@ const ActionGroupTop = observer((props: ICameraViewProps) => {
           borderRadius: 8,
           marginHorizontal: 10,
           marginVertical: 20,
-          flexDirection: "row",
+          flexDirection: 'row',
           height: 45,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <ActionFlash flashMode={flashMode} switchFlashMode={switchFlashMode} />
         <View
           style={{
@@ -197,26 +202,25 @@ const ActionGroupBottom = observer((props: ICameraViewProps) => {
     withCompress,
   } = props;
   const Theme: ITheme = useTheme() as any;
-  const { switchType } = generateTools();
-  const { actionSnap, actionReset, actionPick } = generateActions(
+  const {switchType} = generateTools();
+  const {actionSnap, actionReset, actionPick} = generateActions(
     camera,
     meta,
     option,
-    withCompress
+    withCompress,
   );
 
   return (
     <View
       style={{
-        position: "absolute",
+        position: 'absolute',
         left: 0,
         right: 0,
-        bottom: 0,
-        backgroundColor: "transparent",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+        bottom: 30,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
       <View
         shadow
         style={{
@@ -225,12 +229,11 @@ const ActionGroupBottom = observer((props: ICameraViewProps) => {
           borderRadius: 8,
           marginHorizontal: 10,
           marginVertical: 20,
-          flexDirection: "row",
+          flexDirection: 'row',
           height: 45,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <ActionSwitchCam switchType={switchType} meta={meta} />
         <ActionSnap
           actionSnap={actionSnap}
@@ -248,7 +251,7 @@ const ActionGroupBottom = observer((props: ICameraViewProps) => {
   );
 });
 
-const ActionSnap = observer(({ actionSnap, actionReset, meta }: any) => {
+const ActionSnap = observer(({actionSnap, actionReset, meta}: any) => {
   const Theme: ITheme = useTheme() as any;
 
   if (!!meta.tempURI) {
@@ -260,8 +263,7 @@ const ActionSnap = observer(({ actionSnap, actionReset, meta }: any) => {
           paddingHorizontal: 5,
           margin: 0,
         }}
-        onPress={actionReset}
-      >
+        onPress={actionReset}>
         <Icon name="md-refresh" color={Theme.colors.text} size={30} />
       </Button>
     );
@@ -279,23 +281,22 @@ const ActionSnap = observer(({ actionSnap, actionReset, meta }: any) => {
         backgroundColor: Theme.colors.secondary,
       }}
       disabled={meta.loading}
-      onPress={actionSnap}
-    >
+      onPress={actionSnap}>
       <View
         style={{
           backgroundColor: Theme.colors.primary,
           width: 60,
           height: 60,
           borderRadius: 999,
-          justifyContent: "center",
-          alignItems: "center",
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       />
     </Button>
   );
 });
 
-const ActionSwitchCam = observer(({ switchType, meta }: any) => {
+const ActionSwitchCam = observer(({switchType, meta}: any) => {
   const Theme: ITheme = useTheme() as any;
 
   if (!!meta.tempURI) return null;
@@ -308,15 +309,14 @@ const ActionSwitchCam = observer(({ switchType, meta }: any) => {
         paddingHorizontal: 5,
         margin: 0,
       }}
-      onPress={switchType}
-    >
+      onPress={switchType}>
       <Icon name="ios-camera-reverse" color={Theme.colors.text} size={34} />
     </Button>
   );
 });
 
 const ActionPickImage = observer(
-  ({ meta, actionPick, disableImagePicker }: any) => {
+  ({meta, actionPick, disableImagePicker}: any) => {
     const Theme: ITheme = useTheme() as any;
 
     if (!!disableImagePicker || !!meta.tempURI) return null;
@@ -329,15 +329,14 @@ const ActionPickImage = observer(
           paddingHorizontal: 5,
           margin: 0,
         }}
-        onPress={actionPick}
-      >
+        onPress={actionPick}>
         <Icon name="ios-images" color={Theme.colors.text} size={28} />
       </Button>
     );
-  }
+  },
 );
 
-const ActionRatio = observer(({ ratio, switchRatio }: any) => {
+const ActionRatio = observer(({ratio, switchRatio}: any) => {
   const Theme: ITheme = useTheme() as any;
 
   return (
@@ -348,20 +347,18 @@ const ActionRatio = observer(({ ratio, switchRatio }: any) => {
         paddingHorizontal: 5,
         margin: 0,
       }}
-      onPress={switchRatio}
-    >
+      onPress={switchRatio}>
       <Text
         style={{
           fontFamily: Theme.fontStyle.bold,
-        }}
-      >
+        }}>
         [{ratio}]
       </Text>
     </Button>
   );
 });
 
-const ActionFlash = observer(({ flashMode, switchFlashMode }: any) => {
+const ActionFlash = observer(({flashMode, switchFlashMode}: any) => {
   const Theme: ITheme = useTheme() as any;
 
   return (
@@ -372,19 +369,17 @@ const ActionFlash = observer(({ flashMode, switchFlashMode }: any) => {
         paddingHorizontal: 5,
         margin: 0,
       }}
-      onPress={switchFlashMode}
-    >
-      {flashMode === "auto" ? (
+      onPress={switchFlashMode}>
+      {flashMode === 'auto' ? (
         <Text
           style={{
             fontFamily: Theme.fontStyle.bold,
-          }}
-        >
+          }}>
           Auto
         </Text>
       ) : (
         <Icon
-          name={flashMode === "on" ? "ios-flash" : "ios-flash-off"}
+          name={flashMode === 'on' ? 'ios-flash' : 'ios-flash-off'}
           color={Theme.colors.text}
           size={24}
         />
@@ -393,7 +388,7 @@ const ActionFlash = observer(({ flashMode, switchFlashMode }: any) => {
   );
 });
 
-const ActionSave = observer(({ onSave, meta }: any) => {
+const ActionSave = observer(({onSave, meta}: any) => {
   const Theme: ITheme = useTheme() as any;
 
   if (!meta.tempURI) return null;
@@ -408,18 +403,16 @@ const ActionSave = observer(({ onSave, meta }: any) => {
         height: 70,
         backgroundColor: Theme.colors.primary,
       }}
-      onPress={onSave}
-    >
+      onPress={onSave}>
       <View
         style={{
           backgroundColor: Theme.colors.secondary,
           width: 60,
           height: 60,
           borderRadius: 999,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <Icon name="md-checkmark" color={Theme.colors.textLight} size={34} />
       </View>
     </Button>
